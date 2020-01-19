5040e56c04c937f3b8835d6e567ae05b304c4110
HarshalTalreja <talrejaharshal50@gmail.com>
2020-01-19 23:21:02 +0530

    DOM COMPLETED

--- DOM/Project.js
+++ DOM/Project.js
@@ -1,6 +1,26 @@
 var restart_button = document.querySelector("#restartButton");
 var table_cell = document.querySelectorAll("td");
 
-restartButton.addEventListener('click', function(){
-  restartButton.textContent = "X";
-})
+function clearBoard() {
+  for (var i = 0; i < table_cell.length; i++) {
+    table_cell[i].textContent = '';
+  }
+}
+
+function changeTile(tile) {
+  if (this.textContent === '') {
+    this.textContent = 'X';
+  }
+  else if (this.textContent === 'X') {
+    this.textContent = 'O';
+  }
+  else {
+    this.textContent = '';
+  }
+}
+
+restartButton.addEventListener('click', clearBoard)
+
+for (var i = 0; i < table_cell.length; i++) {
+  table_cell[i].addEventListener('click', changeTile)
+}
